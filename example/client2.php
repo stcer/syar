209e9ee9bcc8e74e7c77db50d5bf1fce801b5b56
stcer <7443930@qq.com>
2016-11-22 15:00:16 +0800

    add Yar_Concurrent_Client test

---
+++ example/client2.php
@@ -0,0 +1,27 @@
+<?php
+
+$vendorPath = realpath(__DIR__ . "/../vendor/");
+$loader = include($vendorPath . "/autoload.php");
+
+$data = [];
+
+/** @var \syar\example\service\Test $news */
+$url = 'http://127.0.0.1:5604/test/';
+for($i = 0; $i < 10; $i++){
+    Yar_Concurrent_Client::call($url, "getName", [rand(0, 245301)],
+        function($rs, $callinfo) use ($i, & $data){
+            $data['name_' . $i] = $rs;
+        }
+    );
+
+    Yar_Concurrent_Client::call($url, "getAge", [],
+        function($rs, $callinfo) use ($i, & $data){
+            $data['age_' . $i] = $rs;
+        }
+    );
+}
+Yar_Concurrent_Client::loop();
+
+
+echo "<pre>";
+var_dump($data);
